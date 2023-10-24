e28cdf6d73451e8440d3649fed106394e9972696
sebastian-xyz <github@sjng.de>
2023-10-24 14:54:19 +0200

    Output C++ bool array

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var $table;
 var rowMajor = false;
 var msbendian = false;
 
-$(function() {
+$(function () {
 
-	matrix = createArray(16,16);
+	matrix = createArray(16, 16);
 	updateTable();
 	initOptions();
 
@@ -21,43 +21,43 @@ function updateTable() {
 
 	// events
 	$table.on("mousedown", "td", toggle);
-    $table.on("mouseenter", "td", toggle);
-    $table.on("dragstart", function() { return false; });
+	$table.on("mouseenter", "td", toggle);
+	$table.on("dragstart", function () { return false; });
 }
 
 function initOptions() {
-	$('#clearButton').click(function() { matrix = createArray(matrix.length,matrix[0].length); updateTable(); $('#_output').hide(); });
+	$('#clearButton').click(function () { matrix = createArray(matrix.length, matrix[0].length); updateTable(); $('#_output').hide(); });
 	$('#generateButton').click(updateCode);
 
-	 $('#widthDropDiv li a').click(function () {
-	 	var width = parseInt($(this).html());
-	 	var height = matrix.length;
-        matrix = createArray(height, width);
-        updateTable();
-        updateSummary();
-     });
+	$('#widthDropDiv li a').click(function () {
+		var width = parseInt($(this).html());
+		var height = matrix.length;
+		matrix = createArray(height, width);
+		updateTable();
+		updateSummary();
+	});
 
-     $('#heightDropDiv li a').click(function () {
-	 	var width = matrix[0].length;
-	 	var height = parseInt($(this).html());
-        matrix = createArray(height, width);
-        updateTable();
-        updateSummary();
-     });
+	$('#heightDropDiv li a').click(function () {
+		var width = matrix[0].length;
+		var height = parseInt($(this).html());
+		matrix = createArray(height, width);
+		updateTable();
+		updateSummary();
+	});
 
-     $('#byteDropDiv li a').click(function () {
-	 	var selection = $(this).html();
-        rowMajor = selection.startsWith("Row");  
-        updateSummary();      	
-     });
+	$('#byteDropDiv li a').click(function () {
+		var selection = $(this).html();
+		rowMajor = selection.startsWith("Row");
+		updateSummary();
+	});
 
-     $('#endianDropDiv li a').click(function () {
-	 	var selection = $(this).html();
-        msbendian = selection.startsWith("Big");  
-        updateSummary();      	
-     });
+	$('#endianDropDiv li a').click(function () {
+		var selection = $(this).html();
+		msbendian = selection.startsWith("Big");
+		updateSummary();
+	});
 
-     updateSummary();
+	updateSummary();
 }
 
 function updateSummary() {
@@ -76,8 +76,20 @@ function updateSummary() {
 
 function updateCode() {
 	$('#_output').show();
+	var width = matrix[0].length;
+	var height = matrix.length;
 	var bytes = generateByteArray();
-	var output = "const uint_8 data[] =\n{\n" + bytes + "\n};"
+	var output = "bool data[" + matrix.length + "]" + "[" + matrix[0].length + "] = {";
+	for (var y = 0; y < height; y++) {
+		output += "{ "
+		for (var x = 0; x < width; x++) {
+			output += bytes[y * width + x] + ", ";
+		}
+		output = output.slice(0, -2);
+		output += " },\n";
+	}
+	output = output.slice(0, -2);
+	output += "};";
 	$('#_output').html(output);
 	$('#_output').removeClass('prettyprinted');
 	prettyPrint();
@@ -97,41 +109,29 @@ function generateByteArray() {
 			if (!temp) temp = 0;
 			// Row Major or Column Major?
 			if (!rowMajor) {
-				buffer[x * height + y] = temp;	
+				if (temp == 0) {
+					buffer[x * height + y] = "false";
+				}
+				else {
+					buffer[x * height + y] = "true";
+				}
 			}
 			else {
-				buffer[y * width + x] = temp;
+				if (temp == 0) {
+					buffer[y * width + x] = "false";
+				}
+				else {
+					buffer[x * height + y] = "true";
+				}
 			}
-			
+
 		}
 	}
 
 	// Read buffer 8-bits at a time
 	// and turn it into bytes
-	for (var i = 0; i < buffer.length; i+=8) {
-		var newByte = 0;
-		for (var j = 0; j < 8; j++) {
-            if (buffer[i+j]) {
-            	if (msbendian) {
-                	newByte |= 1 << (7-j);
-                }
-                else {
-                	newByte |= 1 << j;
-                }
-            }
-        }
-        bytes[i / 8] = newByte;
-	}
 
-	var formatted = bytes.map(function (x) {
-	    x = x + 0xFFFFFFFF + 1;  // twos complement
-	    x = x.toString(16); // to hex
-	    x = ("0"+x).substr(-2); // zero-pad to 8-digits
-	    x = "0x" + x;
-	    return x;
-	}).join(', ');
-
-	return formatted;
+	return buffer;
 }
 
 function toggle(e) {
@@ -140,33 +140,33 @@ function toggle(e) {
 
 	if (e.buttons == 1 && !e.ctrlKey) {
 		matrix[x][y] = 1;
-		$(this).addClass('on');		
+		$(this).addClass('on');
 	}
-	else if (e.buttons == 2 || (e.buttons == 1 && e.ctrlKey)) {			
+	else if (e.buttons == 2 || (e.buttons == 1 && e.ctrlKey)) {
 		matrix[x][y] = 0;
-		$(this).removeClass('on');	
+		$(this).removeClass('on');
 	}
 
 	return false;
 }
 
 function populateTable(table, rows, cells, content) {
-    if (!table) table = document.createElement('table');
-    for (var i = 0; i < rows; ++i) {
+	if (!table) table = document.createElement('table');
+	for (var i = 0; i < rows; ++i) {
 		var row = document.createElement('tr');
 		row.id = i;
-        for (var j = 0; j < cells; ++j) {
+		for (var j = 0; j < cells; ++j) {
 			var tr = document.createElement('td');
-			tr.id = j ;
-			tr.setAttribute('onmouseover' , "coordinates(" + tr.id +','+ row.id + ");");
-            row.appendChild(tr);
-            $(row.cells[j]).data('i', i);
-            $(row.cells[j]).data('j', j);
-        }
-        table.appendChild(row);        
-    }
-    $table = $(table);
-    return table;
+			tr.id = j;
+			tr.setAttribute('onmouseover', "coordinates(" + tr.id + ',' + row.id + ");");
+			row.appendChild(tr);
+			$(row.cells[j]).data('i', i);
+			$(row.cells[j]).data('j', j);
+		}
+		table.appendChild(row);
+	}
+	$table = $(table);
+	return table;
 }
 
 // (height, width)
@@ -174,24 +174,24 @@ function createArray(length) {
 
 	var arr = new Array(length || 0),
 		i = length;
-		
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
-    }
 
-    return arr;
+	if (arguments.length > 1) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+	}
+
+	return arr;
 }
 
 
 
 var add_height = document.getElementById("add_height");
-add_height.addEventListener("keyup", function(event) {
+add_height.addEventListener("keyup", function (event) {
 	if (event.keyCode === 13) {
 		event.preventDefault();
 		var height_ul = document.getElementById('dropdown_height');
 		var height_li = document.createElement("li");
-		var height_a  = document.createElement("a");
+		var height_a = document.createElement("a");
 		height_a.href = "#";
 		height_a.innerText = add_height.value;
 		height_li.appendChild(height_a);
@@ -203,23 +203,23 @@ add_height.addEventListener("keyup", function(event) {
 
 
 var add_width = document.getElementById("add_width");
-add_width.addEventListener("keyup", function(event) {
+add_width.addEventListener("keyup", function (event) {
 	if (event.keyCode === 13) {
 		event.preventDefault();
 		var width_ul = document.getElementById('dropdown_width');
 		var width_li = document.createElement("li");
-		var width_a  = document.createElement("a");
+		var width_a = document.createElement("a");
 		width_a.href = "#";
 		width_a.innerText = add_width.value;
 		width_li.appendChild(width_a);
 		width_ul.prepend(width_li);
-	
+
 		initOptions();
 	}
 });
 
 
-function coordinates(row , col){
+function coordinates(row, col) {
 	document.getElementById('coor_row').innerText = row;
 	document.getElementById('coor_col').innerText = col;
 }
